14dace0d842bafd76a7d0d9608da56cc33c1e819
Jagger85 <andresfonsortega@yahoo.es>
2024-12-15 16:20:13 +0700

    Feat(sock_route): Added video url args to the request

--- routes/speecher/speecher.py
+++ routes/speecher/speecher.py
@@ -13,13 +13,17 @@ bp_speecher = Blueprint("speecher", __name__)
 def index():
     if not request.args.get("client_id"):
         return jsonify({"error": "client_id is required"}), 400
+    elif not request.args.get("video_url"):
+        return jsonify({"error": "video_url is required"}), 400
     else:
         client_id = request.args.get("client_id")
+        video_url = request.args.get("video_url")
+        task_id = uuid.uuid4()
         # Start the task
-        task = test_task.delay(uuid.uuid4(), client_id)
+        task = test_task.delay(task_id, client_id, video_url)
         
     # Wait for the task to complete and get the result
-    result = task.get(timeout=1000)  # Adjust timeout as needed
+    result = task.get(timeout=3000)  # Adjust timeout as needed
     
     # Return the result as a JSON response
     return jsonify({"result": result})
